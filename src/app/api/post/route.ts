058fc556caa4e53600c0af159d9e08c460e93d14
Auzan <istikhoriauzan@gmail.com>
2023-05-03 19:24:04 +0700

    feat: post request

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -7,3 +7,14 @@ export const GET = async (req: NextRequest) => {
   const post = await prisma.post.findMany({});
   return NextResponse.json({ post });
 };
+
+export const POST = async (req: NextRequest) => {
+  const { title, content } = req.json();
+  const post = await prisma.post.create({
+    data: {
+      title,
+      content,
+    },
+  });
+  return NextResponse.json({ post });
+};
